021cc2644a389c897f8cb8b4197480f27d12fc36
TrentB <trentjblackwell@gmail.com>
2019-04-09 19:38:27 -0700

    created game logic

---
+++ memory_game/js/index.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
+
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardFour);
 
+var cards = ["Queen", "Queen", "King", "King"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) { 
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+ 	alert("You found a match!");
+ }
+ 	else { 
+ 		alert("Sorry, try again.");
+}
+}
